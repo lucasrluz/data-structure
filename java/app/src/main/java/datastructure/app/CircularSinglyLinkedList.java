0a798cd6aba854ddc217424217a2516c67d33c7e
lucasrluz <lucasr.luz42@gmail.com>
2023-11-05 18:40:57 -0300

    feat: adicionado comentários breves com explicação

--- java/app/src/main/java/datastructure/app/CircularSinglyLinkedList.java
+++ java/app/src/main/java/datastructure/app/CircularSinglyLinkedList.java
@@ -13,6 +13,7 @@ public class CircularSinglyLinkedList {
 	public Node head;
 
 	public void add(String data) {
+		// Caso a lista esteja vazia.
 		if (this.head == null) {
 			Node newNode = new Node(data);
 
@@ -20,7 +21,8 @@ public class CircularSinglyLinkedList {
 
 			return;
 		}
-
+		
+		// Caso tenha um ou mais elementos na lista.
 		Node current = this.head;
 
 		while(true) {
@@ -33,7 +35,8 @@ public class CircularSinglyLinkedList {
 
 				break;
 			}
-
+			
+			// Quando chega no final da lista.
 			if (current.next == this.head) {
 				Node newNode = new Node(data);
 
@@ -48,16 +51,20 @@ public class CircularSinglyLinkedList {
 	}
 
 	public void remove(String data) {
+		// Caso a lista esteja vazia.
 		if (this.head == null) {
 			return;
 		}	
 
+		// Caso tenha um elemento na lista e seja o que queremos remover.
 		if (this.head.next == null && this.head.data == data) {
 			this.head = null;
 			return;
 		}
 
+		// Caso queremos remover o primeiro elemento da lista e tenha mais de um elemento adicionado. 
 		if (this.head.next != null && this.head.data == data) {
+			// Encontra o último Node e aponta ele para o novo Node do head.
 			Node current = this.head;
 
 			while(true) {
@@ -74,11 +81,13 @@ public class CircularSinglyLinkedList {
 			return;
 		}
 
+		// Caso tenha dois elementos na lista e queremos remover o último elemento.
 		if (this.head.next.next == this.head && this.head.next.data == data) { 
 			this.head.next = null;
 			return;
 		}
 
+		// Caso tenha mais de dois elementos na lista e queremos remover um elemento depois do segundo.
 		Node current = this.head;
 
 		while(true) {
@@ -97,25 +106,31 @@ public class CircularSinglyLinkedList {
 	}
 
 	public boolean get(String data) {
+		// Caso a lista esteja vazia.
 		if (this.head == null) {
 			return false;
 		}
 
+		// Caso tenha um elemento na lista.
 		if (this.head.data == data) {
 			return true;
 		}
 
+		// Caso tenha mais de um elemento na lista.
 		Node current = this.head;	
 
 		while(true) {
+			// Caso tenha dois elemento na lista e queremos remover o último elemento.
 			if (current.next == this.head && current.data == data) {
 				return true;
 			}	
 
+			// Caso tenha dois elementos na lista e não encontre o elemento para remover.
 			if (current.next == this.head && current.data != data) {
 				return false;
 			}
 
+			// Caso tenha mais de dois elementos na lista e encontre o elemento para remover.
 			if (current.next.data == data) {
 				return true;
 			}
@@ -125,18 +140,21 @@ public class CircularSinglyLinkedList {
 	}
 
 	public String get() {
+		// Caso a lista esteja vazia.
 		if (this.head == null) {
 			return "";
 		}
 
 		String circularSinglyLinkedList = this.head.data + " ";
 
+		// Caso tenha um elemento na lista.
 		if (this.head.next == null) {
 			return circularSinglyLinkedList;
 		}
 
 		Node current = this.head.next;	
-
+	
+		// Caso tenha mais de um elemento na lista.
 		while(true) {
 			circularSinglyLinkedList += current.data + " ";
 
--- java/app/src/main/java/datastructure/app/SinglyLinkedList.java
+++ java/app/src/main/java/datastructure/app/SinglyLinkedList.java
@@ -13,6 +13,7 @@ public class SinglyLinkedList {
 	public Node head;
 
 	public void add(String data) {
+		// Caso a lista esteja vazia.
 		if (this.head == null)	{
 			Node newNode = new Node(data);
 
@@ -21,6 +22,7 @@ public class SinglyLinkedList {
 			return;
 		}
 
+		// Caso tenha mais de um elemento na lista.
 		Node current = this.head;
 
 		while(true) {
@@ -37,20 +39,24 @@ public class SinglyLinkedList {
 	}
 
 	public void remove(String data) {
+		// Caso a lista esteja vazia.
 		if (this.head == null) {
 			return;
 		}
 
+		// Caso o elemento para remover seja o primeiro e a lista tenha um elemento.
 		if (this.head.data.equals(data) && this.head.next == null) {
 			this.head = null;
 			return;
 		}
 
+		// Caso o elemento para remover seja o primeiro e a lista tenha mais de um elemento.
 		if (this.head.data.equals(data) && this.head.next != null) {
 			this.head = this.head.next;
 			return;
 		}
 		
+		// Caso a lista tenha mais de um elemento e vamos remover um elemento do meio ou do final.
 		Node current = this.head;
 
 		while(true) {
@@ -72,10 +78,12 @@ public class SinglyLinkedList {
 		Node current = this.head;	
 
 		while(true) {
+			// Caso a lista esteja vazia.
 			if (current == null) {
 				return false;
 			}
 
+			// Caso ache o valor.
 			if (current.data.equals(data)) {
 				return true;
 			}
@@ -90,6 +98,7 @@ public class SinglyLinkedList {
 		Node current = this.head;
 
 		while(true) {
+			// Caso a lista esteja vazia.
 			if (current == null) {
 				return linkedList;
 			}
